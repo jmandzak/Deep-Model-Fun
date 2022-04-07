18d313b81179ffa6cee483de0d7516f90fb68ce9
Josh Mandzak <jmandzak99@gmail.com>
2022-04-06 22:30:38 -0400

    task 4 mostly working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras import layers, losses, metrics, optimizers
 import cv2
 import numpy as np
@@ -56,6 +56,49 @@ def task1(train_img, train_class, val_img, val_class, target):
     print(model.evaluate(val_img, y_test))
 
 
+# train a CNN that has two branches at the end to determine two different attributes
+def task4(train_img, train_class, val_img, val_class, target):
+
+    # for now let's just assume the two tasks are always gender and race
+    y_train_gender = pd.get_dummies(train_class.gender)
+    y_train_race = pd.get_dummies(train_class.race)
+
+    y_test_gender = pd.get_dummies(val_class.gender)
+    y_test_race = pd.get_dummies(val_class.race)
+
+    # make each layer
+    input = layers.Input(shape=(32,32,1))
+    conv1 = layers.Conv2D(filters=40, kernel_size=(5,5), padding='valid', strides=1, activation='relu', name='conv1')(input)
+    max1 = layers.MaxPool2D(pool_size=(2,2), name='max1')(conv1)
+    flatten = layers.Flatten(name='flatten')(max1)
+    fc1 = layers.Dense(100, activation='relu', name='fc1')(flatten)
+    fc2 = layers.Dense(100, activation='relu', name='fc2')(flatten)
+    out1 = layers.Dense(2, activation='softmax', name='out1')(fc1) # assuming task1 is gender
+    out2 = layers.Dense(7, activation='softmax', name='out2')(fc2) # assuming task2 is race
+
+    # now make the model
+    model = Model(inputs=input, outputs=[out1, out2], name='model')
+
+    # define the different losses + other hyperparams
+    losses = {
+	    "out1": "categorical_crossentropy",
+	    "out2": "categorical_crossentropy",
+    }
+    batch_size = 100
+    lr = 0.001
+    optimizer = optimizers.Adam(learning_rate=lr)
+
+    model.compile(optimizer=optimizer, loss=losses, metrics=['accuracy'])
+
+    # create tensorboard
+    #creating unique name for tensorboard directory
+    log_dir = "logs/class/" + datetime.datetime.now().strftime(f"%Y-%m-%d-%H:%M:%S-batch_size={batch_size}-lr={lr}")
+    #Tensforboard callback function
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    model.fit(train_img, {'out1': y_train_gender, 'out2': y_train_race}, batch_size=batch_size, epochs=50, validation_data=(val_img, {'out1': y_test_gender, 'out2': y_test_race}), callbacks=[tensorboard_callback])
+
+    print(model.evaluate(val_img, {'out1': y_test_gender, 'out2': y_test_race}))
 
 def main():
     # do command line arguments here
@@ -98,6 +141,9 @@ def main():
     task = sys.argv[1]
     if(task == '1'):
         task1(train_images, train_labels, val_images, valid_labels, target)
+
+    elif(task == '4'):
+        task4(train_images, train_labels, val_images, valid_labels, target)
 
 
 
